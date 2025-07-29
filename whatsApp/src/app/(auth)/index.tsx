0c169dd6e8c0b534df1825fcefebbe1ca380bcd2
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2025-07-29 18:29:15 +0530

    create first page react native

--- whatsApp/src/app/(auth)/index.tsx
+++ whatsApp/src/app/(auth)/index.tsx
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
 import React from 'react'
+import imagePath from '@/src/constants/imagePath'
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 
 const Auth = () => {
   return (
-    <View>
-      <Text> index </Text>
+    <SafeAreaView  style={styles.container}> 
+    <View  style={styles.header}></View>
+    <View  style={styles.body}>
+
+
+      <Image source={imagePath.logo} style={styles.myimage}/>
     </View>
+    <View  style={styles.footer}>
+
+      <Text style={styles.fromtext}>From</Text>
+      <Text  style={styles.facebooktext}>facebook</Text>
+    </View>
+    </SafeAreaView>
   )
 }
 
-export default Auth
+const styles = StyleSheet.create({
+  container:{
+  flex:1, justifyContent:'space-between', alignItems:'center', paddingVertical:moderateScale(60),  
+  },
+  header:{ 
+  },
+  body:{ 
+    width:100
+  },
+  footer:{
+  alignItems:'center'
+  },
+
+
+  fromtext:{
+    fontSize:12, 
+    color:'#867373'
+
+
+  },
+  facebooktext:{
+ fontSize:15,  
+  }  ,
+  myimage:{
+    width:moderateScale(90), 
+    height:moderateScale(90), 
+    boxSizing:'border-box'
+  }
+})
+export default Auth   

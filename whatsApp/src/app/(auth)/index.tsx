73498cf9dd4d71955f1a1df64eacf8721f2c912a
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2025-07-30 14:53:55 +0530

    change styles

--- whatsApp/src/app/(auth)/index.tsx
+++ whatsApp/src/app/(auth)/index.tsx
@@ -25,7 +25,6 @@ const styles = StyleSheet.create({
   container:{
   flex:1, justifyContent:'space-between', alignItems:'center', paddingVertical:moderateScale(60),  
   }, 
-  
   header:{ 
   },  
   body:{ 

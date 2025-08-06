837a6996d2a28c0f92e27a81016cce7972eb1295
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2025-08-06 15:15:36 +0530

    change tags

--- whatsApp/src/app/(auth)/index.tsx
+++ whatsApp/src/app/(auth)/index.tsx
@@ -1,19 +1,34 @@
-import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Image, ActivityIndicator } from 'react-native'
+import React, { useState } from 'react'
 import imagePath from '@/src/constants/imagePath'
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 
 const Auth = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <SafeAreaView  style={styles.container}> 
     <View style={styles.header} ></View>
-    <View style={styles.body}> 
-      <Image source={imagePath.logo} style={styles.myimage}/>
+    <View style={styles.body} > 
+      <Image source={imagePath.logo} style={styles.myimage} />
+
+      <Text style={styles.whatsapp_text}>WhatsApp</Text>
     </View>
     <View  style={styles.footer}>
 
-      <Text style={styles.fromtext}>From</Text>
+      {
+        isLoading ? (
+          <> 
+          <ActivityIndicator size={moderateScale(50)} color="#0ccc83" />
+          </>
+        ) : (
+          <>
+          <Text style={styles.fromtext}>From</Text>
       <Text  style={styles.facebooktext}>facebook</Text>
+      </>
+        )
+      }
+
+      
     </View>
     </SafeAreaView>
   )
@@ -34,19 +49,22 @@ const styles = StyleSheet.create({
   footer:{
   alignItems:'center'
   }, 
-
   fromtext:{
     fontSize:12,  
     color:'#867373' 
   },
   facebooktext:{
   fontSize:15,
-    
   }, 
   myimage:{
     width:moderateScale(90), 
     height:moderateScale(90), 
     boxSizing:'border-box'
+  },
+  whatsapp_text:{
+    fontSize:moderateScale(20), 
+    fontWeight:'bold', 
+    color:'#000000'
   }
 })
 
